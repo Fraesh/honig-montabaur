113a8b6025db196dfa120768e9de4d4dcb851167
fraesh <fraesh93@googlemail.com>
2026-04-27 12:45:09 +0200

    koniginnenblock fix

--- src/routes/components/KoeniginnenBlock.svelte
+++ src/routes/components/KoeniginnenBlock.svelte
@@ -1,9 +1,10 @@
 <script>
+	import { goto } from '$app/navigation';
 	import bgImage from '$lib/assets/img/bienenwabe.webp';
 	import Hex from '$lib/components/ornaments/Hex.svelte';
 </script>
 
-<div class="relative w-full p-24 overflow-hidden">
+<div class="relative w-full p-10 md:p-24 overflow-hidden">
 	<!-- Background image -->
 	<div class="absolute inset-0 w-full h-full z-0">
 		<img src={bgImage} alt="Bienenwabe" class="w-full h-full object-cover" />
@@ -13,11 +14,11 @@
 
 	<!-- Content -->
 	<div class="relative z-10 container mx-auto px-4 flex flex-col items-start">
-		<h1 class="text-3xl md:text-4xl font-bold mb-8 text-white leading-tight miami">
+		<h1 class="text-3xl md:text-4xl font-bold mb-8 text-white leading-normal miami">
 			Wie wir gesunde Bienenvölker heranziehen
 		</h1>
 
-		<a href="/königinnen" class="btn btn-primary font-bold border-none">
+		<a href="/königinnen" class="btn btn-primary font-bold border-none" target="_self">
 			Mehr zur Königinnenzucht
 		</a>
 	</div>
